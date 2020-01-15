095dffc80e95ca72eed278b0ff28fc11cf4105f5
Wendell P. Barreto <wendell@tootz.com.br>
2020-01-15 00:19:22 -0200

    feat: add create dev route and register on database

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,33 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-routes.get('/users', (req, res) => {
-  return res.json({
-    users: []
+routes.post('/devs', async (req, res) => {
+  const { github_username, techs, latitude, longitude } = req.body;
+
+  const response = await axios.get(
+    `https://api.github.com/users/${github_username}`
+  );
+
+  const { name = login, avatar_url, bio } = response.data;
+  const techsArray = techs.split(',').map(tech => tech.trim());
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude]
+  };
+
+  const resDev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray,
+    location
   });
+
+  return res.json(resDev);
 });
 
 module.exports = routes;
